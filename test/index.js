4096eb43004880f731235e8890b84011e961c615
Matt <matthewwilshire1@gmail.com>
2023-04-22 00:24:43 +0100

    Use Unpsent TXOs to send and combine them if needed

--- test/index.js
+++ test/index.js
@@ -28,19 +28,42 @@ function btcToSatoshi(btc) {
     return Math.floor(btc * 100000000);
 }
 
+function satoshiToBtc(sat) {
+    return sat / 100000000;
+}
+
+async function getUTXOs(address, completed=true) {
+    try {
+        let response = await axios.get(`https://blockstream.info/testnet/api/address/${address}/utxo`);
+        let data = response.data;
+
+        if(completed) {
+            data = data.filter((d) => d.status.confirmed);
+        }
+        
+        return data;
+    } catch(error) {
+        console.log("Cannot get UTXOs of address, is the address correct ?");
+        return null;
+    }
+}
+
+async function getBalance(address) {
+    let utxos = await getUTXOs(address);
+    let balance = utxos.reduce((accumulator, object) => accumulator + object.value, 0);
+    return balance;
+}
+
 async function bal() {
     let address = prompt('Address: ');
-    try {
-        let response = await axios.get(`https://blockstream.info/testnet/api/address/${address}`);
-		let data = response.data;
-	
-		let received = data.chain_stats.funded_txo_sum;
-		let spent = data.chain_stats.spent_txo_sum;
-		let totalAmount = received - spent;
-        console.log(`Amount @ ${address} : ${totalAmount} : ${totalAmount / 100000000}`);
-    } catch(error) {
-        console.log(error);
+    let utxos = await getUTXOs(address);
+
+    for(let utxo of utxos) {
+        console.log(`UTXO -> ${utxo.txid} -> ${utxo.value} : ${satoshiToBtc(utxo.value)}`);
     }
+
+    let balance = await getBalance(address);
+    console.log(`Amount @ ${address} : ${balance} : ${satoshiToBtc(balance)}`);
 }
 
 async function send() {
@@ -55,20 +78,11 @@ async function send() {
 		TODO: Change this to use UTXO e.g https://blockstream.info/testnet/api/address/{address}/utxo, combine all UTXO values to get unspent
         These can be used as multiple inputs to the transactions as you can have the sum of two received transactions and send it to someone.
 	*/
-    let totalAmount;
-    try {
-        let response = await axios.get(`https://blockstream.info/testnet/api/address/${address}`);
-		let data = response.data;
-	
-		let received = data.chain_stats.funded_txo_sum;
-		let spent = data.chain_stats.spent_txo_sum;
-		totalAmount = received - spent;
-    } catch(error) {
-        console.log(error);
-    }
+    let totalAmount = await getBalance(address);
 
-    let amountToSend = prompt(`There is ${totalAmount / 100000000} in this address (${address}), how much do you want to send ? `);
-    let amount = parseFloat(amountToSend);
+    let amountToSend = prompt(`There is ${totalAmount / 100000000} in this address (${address}), how much do you want to send (In Satoshis) ? `);
+    // let amount = parseFloat(amountToSend);
+    let amount = parseInt(amountToSend);
     console.log(amount);
 
     let sendAddress = prompt(`Send Address: `);
@@ -78,53 +92,59 @@ async function send() {
 
     let changeAddress = prompt(`Change Address: `);
 
-    let txHex = await createTransaction(keyPair, address, sendAddress, changeAddress, amount, totalAmount);
+    let txHex = await createTransaction(keyPair, address, sendAddress, changeAddress, amount);
 
     try {
-        let resp = axios.post('https://blockstream.info/testnet/api/tx', txHex);
+        let resp = await axios.post('https://blockstream.info/testnet/api/tx', txHex);
         console.log(`Transaction Broadcasted TXID: ${resp.data}`)
     } catch(error) {
         console.log(error);
     }
 }
 
-async function createTransaction(keyPair, fromAddress, sendAddress, changeAddress, amount, totalAmount) {
+async function createTransaction(keyPair, fromAddress, sendAddress, changeAddress, amount) {
 	const transaction = new bitcoin.TransactionBuilder(network);
     const fee = 1000;
 
-	//Get last transaction ID and the position of the transaction inside the block
-	let response = await axios.get(`https://blockstream.info/testnet/api/address/${fromAddress}/txs`);
-	let data = response.data;
-	// Get txs that are confirmed
-	data = data.filter((d) => d.status.confirmed);
+    let allUtxos = await getUTXOs(fromAddress);
 
-	if(data.length < 1) {
-		console.log("No past transaction ID, this address has never had BTC ?");
-		process.exit();
-	}
+    let utxos = [];
+    let accumAmount = 0;
+    let i = 0;
 
-	let tx = data[0];
-	let txid = tx.txid;
-	let txIndex = 0;
-
-	// Go through outputs and find which one was directed to my address
-	for(let i = 0; i < tx.vout.length; i++) {
-		if(tx.vout[i].scriptpubkey_address == fromAddress) {
-			txIndex = i;
-			break;
-		}
-	}
-
-	transaction.addInput(txid, txIndex);
-	
-    if(changeAddress != '') {
-        transaction.addOutput(sendAddress, btcToSatoshi(amount));
-        transaction.addOutput(changeAddress, totalAmount - btcToSatoshi(amount) - fee);
-    } else {
-        transaction.addOutput(sendAddress, btcToSatoshi(amount) - fee);
+    // Add up utxos to get above the sending amount
+    while(accumAmount < amount + fee) {
+        utxos.push(allUtxos[i]);
+        accumAmount += allUtxos[i].value;
+        i++;
     }
 
-	transaction.sign(0, keyPair);
+    console.log(`Amount to send is ${amount}`);
+
+    let changeToSend = accumAmount - amount - fee;
+    console.log(`Amount sent to change address is ${changeToSend} (this is with fees taken away)`);
+    console.log(`Amount of UTXOS to be used ${utxos.length}`);
+
+    let confirm = prompt(`Send ? y/n: `);
+
+    if(confirm != 'y') return null;
+    
+    for(let utxo of utxos) {
+        // Add previous UTXOs as input
+        transaction.addInput(utxo.txid, utxo.vout);
+    }
+	
+    if(changeAddress != '') {
+        transaction.addOutput(sendAddress, amount);
+        transaction.addOutput(changeAddress, accumAmount - amount - fee);
+    } else {
+        transaction.addOutput(sendAddress, amount + fee);
+    }
+
+    for(let i = 0; i < utxos.length; i++) {
+        // Sign UTXO inputs
+        transaction.sign(i, keyPair);
+    }
 
 	return transaction.build().toHex();
 }
